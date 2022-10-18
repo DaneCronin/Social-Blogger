0d8b4ef607ea38791ad12c2b4ad22b90c06ecadc
Dane Cronin <danecronin@gmail.com>
2022-10-18 09:31:50 -0600

    added columns to user, post, comment models

--- models/User.js
+++ models/User.js
@@ -1,13 +1,48 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const { FAILSAFE_SCHEMA } = require('js-yaml');
 
 
 class User extends Model {};
 
-User.init({
-
-});
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
 
 
 module.exports = User;
