998bb00ba0e15128ed72b2a125fe494b1a790d2b
Dane Cronin <danecronin@gmail.com>
2022-10-19 12:29:47 -0600

    fixed user route login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -78,42 +78,32 @@ router.post('/', (req, res) => {
 router.post('/login', (req,res) => {
     User.findOne({
         where: {
-            username: req.body.username
+          username: req.body.username
         }
-    })
-    .then(dbUserData => {
-        if(!dbUserData) {
-            res.status(400).json({message: "No user found with that username!"});
-            return;
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that username!' });
+          return;
         }
-        req.session.save(() => {
-            req.session.user_id = dbUserData.id;
-            req.session.username = dbUserData.username;
-            req.session.loggedIn = true;
-
-            res.json({ user: dbUserData, message: "You are now logged in!"})
-        });
-    });
-
-    const validPassword = dbUserData.checkPassword(req.body.password);
     
-    if(!validPassword) {
-        res.status(400).json({message: "Incorrect password!"});
-        return;
-    }  
-    req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-
-        res.json({
-            user: dbUserData,
-            message: 'You are now logged in!'
+        const validPassword = dbUserData.checkPassword(req.body.password);
+    
+        if (!validPassword) {
+          res.status(400).json({ message: 'Incorrect password!' });
+          return;
+        }
+    
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
         });
+      });
     });
-});
 
-
+//Route for user to logout
     router.post('/logout', (req,res) => {
         if(req.session.loggedIn) {
             req.session.destroy(() => {
